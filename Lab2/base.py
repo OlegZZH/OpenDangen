8b86bde02dab84f673f221958e55ca9ac44bc0e4
OlegZZH <egormaslina@gmail.com>
2022-03-09 22:46:04 +0200

    сетка не двигается

--- Lab2/base.py
+++ Lab2/base.py
@@ -13,22 +13,6 @@ class CameraWindow(mglw.WindowConfig):
         self.camera = KeyboardCamera(self.wnd.keys, aspect_ratio=self.wnd.aspect_ratio)
         self.camera_enabled = True
 
-    def key_event(self, key, action, modifiers):
-        keys = self.wnd.keys
-
-        if self.camera_enabled:
-            self.camera.key_input(key, action, modifiers)
-
-        if action == keys.ACTION_PRESS:
-            if key == keys.C:
-                self.camera_enabled = not self.camera_enabled
-                self.wnd.mouse_exclusivity = self.camera_enabled
-                self.wnd.cursor = not self.camera_enabled
-            if key == keys.SPACE:
-                self.timer.toggle_pause()
-
-
-
 
     def mouse_position_event(self, x: int, y: int, dx, dy):
 
--- Lab2/surf_biz.py
+++ Lab2/surf_biz.py
@@ -15,6 +15,7 @@ from random import *
 from OpenGL.GLU import *
 
 size = 100
+color = 255
 
 
 def biz(*args):
@@ -25,8 +26,6 @@ def biz(*args):
         r = np.append(r, (args[0] * tm ** 3) + (3 * u * args[1] * tm ** 2) + (3 * args[2] * tm * u ** 2) + args[
             3] * u ** 3)
     return r
-
-
 
 
 def grid(size, steps):
@@ -51,7 +50,8 @@ class SimpleGrid(CameraWindow):
         self.camera.projection.update(near=0.01, far=100.0)
         self.camera.velocity = 5.0
         self.camera.mouse_sensitivity = 0.3
-        self.points_s = geometry.sphere(radius=0.5, sectors=32, rings=16)
+        self.select_color = np.array([])
+        self.select_index = np.array([])
 
         self.prog = self.load_program(
             vertex_shader=r"C:\Users\Oleg\Dropbox\lab\OpenDangen\Lab2\resources\programs\vertex_shader.glsl",
@@ -62,32 +62,26 @@ class SimpleGrid(CameraWindow):
         self.L_M = self.prog["lookat"]
         self.T_M = self.prog["trans"]
         self.switcher = self.prog["switcher"]
-        self.point=np.array([])
-        self.curu=np.array([])
-        self.index_curv=np.array([])
+        self.point = np.array([])
+        self.curu = np.array([])
+        self.index_curv = np.array([])
+        self.index = np.array([])
         self.add_surf()
-
         self.add_patch()
-
+        self.update_index()
         self.vbo = self.ctx.buffer(grid(5, 15).astype('f4'))
         self.vbo_axis = self.ctx.buffer(axis().astype('f4'))
         self.vbo_points = self.ctx.buffer(self.point.astype('f4'))
-
         self.vbo_curu = self.ctx.buffer(self.curu.astype('f4'))
-        a = np.arange(8*4).reshape(8, 4)
-        index = np.array([])
-        for i in a:
-            index = np.append(index, [i[0], *np.repeat(i[1:-1], 2), i[-1]])
 
-        for j in range(0, len(a[0])):
-            index = np.append(index, [a[:, j][0], *np.repeat(a[:, j][1:-1], 2), a[:, j][-1]])
-
-        self.ibo_line = self.ctx.buffer(index.astype('i4'))
+        self.ibo_line = self.ctx.buffer(self.index.astype('i4'))
         self.ibo_curv = self.ctx.buffer(self.index_curv.astype('i4'))
 
+        print(self.point)
         self.vao_grid = self.ctx.vertex_array(self.prog, self.vbo, 'in_vert')
         self.vao_axis = self.ctx.vertex_array(self.prog, self.vbo_axis, 'in_vert')
-        self.vao_points = self.ctx.vertex_array(self.prog, self.vbo_points, 'in_vert', index_buffer=self.ibo_line)
+        self.vao_points = self.ctx.vertex_array(self.prog, [(self.vbo_points, "3f 3f", 'in_vert', "point_color")],
+                                                index_buffer=self.ibo_line)
         self.vao_curu = self.ctx.vertex_array(self.prog, [(self.vbo_curu, '3f', 'in_vert')], index_buffer=self.ibo_curv)
 
         self.lookat = Matrix44.look_at(
@@ -97,41 +91,118 @@ class SimpleGrid(CameraWindow):
         )
         self.translation = pyrr.matrix44.create_from_translation([0, 0, 0])
 
+    def mouse_press_event(self, x, y, button):
+        print("Mouse button {} pressed at {}, {}".format(button, x, y))
+        w, h = self.window_size
+
+        data = glReadPixels(x, h - y, 1, 1, GL_RGB, GL_UNSIGNED_BYTE)
+        print(data[0], data[1], data[2])
+        print(self.point[:, :, 3:] * 255)
+        if self.select_color.shape[0] != 0:
+            find = np.where((self.point[:, :, 3] * 255).astype(int) == 0)
+            self.point[find[0], find[1], 3:] = self.select_color
+            self.select_color = np.array([])
+
+        if [data[0], data[1], data[2]] != [255, 255, 255] and data[1] == 0 and data[2] == 0:
+            find = np.where((self.point[:, :, 3] * 255).astype(int) == data[0])
+            self.select_color = self.point[find[0], find[1], :][:, 3:]
+            print(self.select_color)
+            self.point[find[0], find[1], 3:] = 0
+
+        self.vbo_points.write(self.point.astype("f4"))
+
+    def key_event(self, key, action, modifiers):
+        keys = self.wnd.keys
+
+        def find():
+            return np.where((self.point[:, :, 3] * 255).astype(int) == 0)
+
+        if self.camera_enabled:
+            self.camera.key_input(key, action, modifiers)
+
+        if action == keys.ACTION_PRESS:
+            if key == keys.C:
+                self.camera_enabled = not self.camera_enabled
+                self.wnd.mouse_exclusivity = self.camera_enabled
+                self.wnd.cursor = not self.camera_enabled
+            if key == keys.SPACE:
+                self.timer.toggle_pause()
+            if not self.camera_enabled and self.select_color.shape[0] != 0:
+                print(action)
+                if key == keys.I:
+                    self.point[find()[0], find()[1], 2] -= 0.1
+                if key == keys.K:
+                    self.point[find()[0], find()[1], 2] += 0.1
+                if key == keys.J:
+                    self.point[find()[0], find()[1], 1] -= 0.1
+                if key == keys.L:
+                    self.point[find()[0], find()[1], 1] += 0.1
+                if key == keys.O:
+                    self.point[find()[0], find()[1], 0] -= 0.1
+                if key == keys.U:
+                    self.point[find()[0], find()[1], 0] += 0.1
+
+                self.curu = np.array([])
+                self.curu = np.append(self.plot_surf(self.point[:, :, :3][::2]).flatten(), self.curu)
+                self.curu = np.append(self.plot_surf(self.point[:, :, :3][1::2]).flatten(), self.curu)
+                self.vbo_curu = self.ctx.buffer(self.curu.astype('f4'))
+                self.vbo_points.write(self.point.astype("f4"))
+            print(self.point[:,:,:3][1::2])
+
+
+
+    def update_index(self):
+        global color
+        index = np.array([])
+        for i in range(self.point.shape[0] * self.point.shape[1]):
+            index = np.append(index, [color / 255, 0, 0])
+            color -= 1
+        self.point = np.append(self.point, index.reshape(self.point.shape), axis=2)
+
     def add_surf(self):
         h, w = 2, 2
         points = np.array([])
+
         for x in range(-h, h):
             for y in range(-w, w):
-                points = np.append(points, [x, y, np.random.random() * 2])  # np.random.random()
-        self.plot_surf(points)
+                points = np.append(points, [x, y, np.random.random() * 2])
+
+        self.curu = np.append(self.plot_surf(points), self.curu)
+        self.point = np.append(points, self.point)
 
     def add_patch(self):
         h, w = 2, 2
         new_points = np.array([])
+
         for x in range(-h, h):
             for y in range(-5, -1):
-                new_points = np.append(new_points, [x, y, np.random.random() * 2])  # np.random.random()
+                new_points = np.append(new_points, [x, y, np.random.random() * 2])
 
         p = new_points.reshape(4, 4, 3)
-        # print(self.point.reshape(4, 4, 3)[:, 0, :])
         p[:, -1, :] = self.point.reshape(4, 4, 3)[:, 0, :]
-        # print(self.point.reshape(4, 4, 3)[:, 1, :])
-        # print(self.point.reshape(4, 4, 3)[:, 0, :] - self.point.reshape(4, 4, 3)[:, 1, :])
-
         p[:, -2, :] = 2 * self.point.reshape(4, 4, 3)[:, 0, :] - self.point.reshape(4, 4, 3)[:, 1, :]
 
-        # p[:, -2, :]=
-        self.plot_surf(p)
-        self.point= np.append(self.point, p)
 
-    def plot_surf(self,points):
+        self.curu = np.append(self.plot_surf(p.flatten()), self.curu)
+        self.plot_index()
+        a = np.arange(8 * 4).reshape(8, 4)
+        for i in a:
+            self.index = np.append(self.index, [i[0], *np.repeat(i[1:-1], 2), i[-1]])
+
+        for j in a.reshape(4, 8).T:
+            self.index = np.append(self.index, [j[0], *np.repeat(j[1:-1], 2), j[-1]])
+        temp = np.reshape(self.point, [int(self.point.size / 12), 4, 3])
+        temp = np.insert(temp, [0, 1, 2, 3], [p[0], p[1], p[2], p[3]], axis=0)
+        self.point = temp
+
+    def plot_surf(self, points):
         curv = []
         curu = []
         for i in points.reshape(4, 4, 3):
-            a = i[0]
-            b = i[1]
-            c = i[2]
-            d = i[3]
+            a = i[0][0:3]
+            b = i[1][0:3]
+            c = i[2][0:3]
+            d = i[3][0:3]
             curv.append(biz(a, b, c, d))
 
         curv = np.array(curv)
@@ -145,13 +216,11 @@ class SimpleGrid(CameraWindow):
 
             curu.append(biz(a, b, c, d))
         curu = np.array(curu)
-        self.point = np.append(points,self.point )
-        self.curu = np.append(curu,self.curu)
-        # print(curu.reshape(size, size, 3))
-        m=int(self.curu.size/(3*size**2))
-        print("curv= ",self.curu.reshape(m*100,100,3))
-        a = np.arange(m*size ** 2).reshape(m*size, size)
-        print(len(a[0]))
+        return curu
+
+    def plot_index(self):
+        m = int(self.curu.size / (3 * size ** 2))
+        a = np.arange(m * size ** 2).reshape(m * size, size)
         index = np.array([])
         for i in a:
             index = np.append(index, [i[0], *np.repeat(i[1:-1], 2), i[-1]])
@@ -159,15 +228,12 @@ class SimpleGrid(CameraWindow):
         for j in a.T:
             index = np.append(index, [j[0], *np.repeat(j[1:-1], 2), j[-1]])
 
-
-        self.index_curv= index
-
-
+        self.index_curv = index
 
     def render(self, time, frame_time):
         self.ctx.clear(1.0, 1.0, 1.0)
         self.ctx.enable_only(moderngl.CULL_FACE | moderngl.DEPTH_TEST)
-        glPointSize(10)
+        glPointSize(15)
         proj = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 1000.0)
         self.P_M.write(proj.astype('f4'))
         self.C_M.write(self.camera.matrix.astype('f4'))
@@ -178,13 +244,13 @@ class SimpleGrid(CameraWindow):
         self.vao_grid.render(moderngl.LINES)
         self.switcher.value = 1
         self.vao_axis.render(moderngl.LINES)
-        self.vao_points.render(moderngl.POINTS)
-        self.vao_points.render(moderngl.LINES)
 
         self.switcher.value = 2
-        # self.vao_curv.render(moderngl.LINES)
-        # self.vao_curu.render(moderngl.POINTS)
         self.vao_curu.render(moderngl.LINES)
+        self.switcher.value = 3
+        self.vao_points.render(moderngl.POINTS)
+        self.switcher.value = 1
+        self.vao_points.render(moderngl.LINES)
 
 
 if __name__ == '__main__':
